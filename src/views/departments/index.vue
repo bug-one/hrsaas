6118a6bd8e2a211091f877a3678ce96aa0340df5
huangweijie <949801851@qq.com>
2020-12-12 16:42:19 +0800

    封装树形组件，解决代码冗余

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,59 +1,19 @@
 <template>
   <el-card class="tree-card">
-    <!-- 用了一个行列布局 -->
-    <el-row type="flex" justify="space-between" align="middle" style="height: 40px">
-      <el-col>
-        <span>江苏传智播客教育科技股份有限公司</span>
-      </el-col>
-      <el-col :span="4">
-        <el-row type="flex" justify="end">
-          <!-- 两个内容 -->
-          <el-col>负责人</el-col>
-          <el-col>
-            <!-- 下拉菜单 element -->
-            <el-dropdown>
-              <span>
-                操作<i class="el-icon-arrow-down" />
-              </span>
-              <!-- 下拉菜单 -->
-              <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item>添加子部门</el-dropdown-item>
-              </el-dropdown-menu>
-            </el-dropdown>
-          </el-col>
-        </el-row>
-      </el-col>
-    </el-row>
+    <treeTools :data="companyTitle" :is-root="true" />
+
     <el-tree :data="companyTree" :props="defaultProps">
-      <el-row slot-scope="scope" type="flex" justify="space-between" align="middle" style="height: 40px;width:100%">
-        <el-col>
-          <span>{{ scope.data.name }}</span>
-        </el-col>
-        <el-col :span="4">
-          <el-row type="flex" justify="end">
-            <!-- 两个内容 -->
-            <el-col>{{ scope.data.manager }}</el-col>
-            <el-col>
-              <!-- 下拉菜单 element -->
-              <el-dropdown>
-                <span>
-                  操作<i class="el-icon-arrow-down" />
-                </span>
-                <!-- 下拉菜单 -->
-                <el-dropdown-menu slot="dropdown">
-                  <el-dropdown-item>添加子部门</el-dropdown-item>
-                </el-dropdown-menu>
-              </el-dropdown>
-            </el-col>
-          </el-row>
-        </el-col>
-      </el-row>
+      <treeTools slot-scope="scope" :data="scope.data" />
     </el-tree>
   </el-card>
 </template>
 
 <script>
+import treeTools from './components/tree-tools'
 export default {
+  components: {
+    treeTools
+  },
   data() {
     return {
       companyTree: [
@@ -80,6 +40,7 @@ export default {
         }
 
       ],
+      companyTitle: { name: '格力电器有限公司', manager: '负责人' },
       defaultProps: {
         label: 'name',
         children: 'children'
