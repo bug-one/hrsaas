a06620437b88e07304c72b9bc9e96a9ee246fcb0
huangweijie <949801851@qq.com>
2020-12-12 16:16:17 +0800

    利用树形组件作用域插槽,渲染相应数据

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -24,7 +24,31 @@
         </el-row>
       </el-col>
     </el-row>
-    <el-tree :data="companyTree" :props="defaultProps" />
+    <el-tree :data="companyTree" :props="defaultProps">
+      <el-row slot-scope="scope" type="flex" justify="space-between" align="middle" style="height: 40px;width:100%">
+        <el-col>
+          <span>{{ scope.data.name }}</span>
+        </el-col>
+        <el-col :span="4">
+          <el-row type="flex" justify="end">
+            <!-- 两个内容 -->
+            <el-col>{{ scope.data.manager }}</el-col>
+            <el-col>
+              <!-- 下拉菜单 element -->
+              <el-dropdown>
+                <span>
+                  操作<i class="el-icon-arrow-down" />
+                </span>
+                <!-- 下拉菜单 -->
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item>添加子部门</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </el-col>
+          </el-row>
+        </el-col>
+      </el-row>
+    </el-tree>
   </el-card>
 </template>
 
@@ -34,16 +58,21 @@ export default {
     return {
       companyTree: [
         {
-          name: '总裁办'
+          name: '总裁办',
+          manager: '董明珠'
         },
         {
-          name: '行政部'
+          name: '行政部',
+          manager: '刘强东'
         },
         {
           name: '财务部',
+          manager: '马云',
           children: [
             { name: '财务核算部' },
-            { name: '税务管理部' }
+            { name: '税务管理部',
+              manager: '马化腾'
+            }
           ]
         },
         {
