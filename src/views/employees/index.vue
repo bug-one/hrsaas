e5f45c2c88ac20e17af42d3e8099bd142667b1a1
huangweijie <949801851@qq.com>
2020-12-16 21:44:26 +0800

    优化样式

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -3,10 +3,11 @@
     <div class="app-container">
       <pageTools>
         <span slot="before">共66条数据</span>
-        <div slot="after">
-          <el-button>导入员工</el-button>
-          <el-button>导出员工</el-button>
-        </div>
+        <template slot="after">
+          <el-button size="small" type="warning">导入</el-button>
+          <el-button size="small" type="danger">导出</el-button>
+          <el-button size="small" type="primary">新增员工</el-button>
+        </template>
       </pageTools>
     </div>
   </div>
