29dff80035fae1911fc2f329237da9125d09aef8
huangweijie <949801851@qq.com>
2020-12-18 18:26:37 +0800

    处理导入excel表格数据，时间格式问题

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -27,7 +27,13 @@ export default {
           const newItem = {}
           for (const key in item) {
             const newKey = dictionary[key]
-            const value = item[key]
+            let value = item[key]
+            if (newKey === 'timeOfEntry' || newKey === 'correctionTime') {
+              value = this.formatDate(value, '/')
+              console.log(value)
+              value = new Date(value)
+              console.log(value)
+            }
             newItem[newKey] = value
           }
           return newItem
@@ -38,6 +44,17 @@ export default {
       } catch (error) {
         console.log(error)
       }
+    },
+    formatDate(numb, format) {
+      const time = new Date((numb - 1) * 24 * 3600000 + 1)
+      time.setYear(time.getFullYear() - 70)
+      const year = time.getFullYear() + ''
+      const month = time.getMonth() + 1 + ''
+      const date = time.getDate() - 1 + ''
+      if (format && format.length === 1) {
+        return year + format + month + format + date
+      }
+      return year + (month < 10 ? '0' + month : month) + (date < 10 ? '0' + date : date)
     }
   }
 
