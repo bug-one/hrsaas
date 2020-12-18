df6bbee1cd815b68e63066764796b0afaab9d0d3
huangweijie <949801851@qq.com>
2020-12-18 11:01:09 +0800

    修改删除提示语

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -101,7 +101,7 @@ export default {
     },
     async delEmployee(id) {
       try {
-        await this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        await this.$confirm('此操作将永久删除该员工, 是否继续?', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
