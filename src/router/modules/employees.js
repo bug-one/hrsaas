193b403a4049bca423f14b57742fa6d5a8574264
huangweijie <949801851@qq.com>
2020-12-19 20:44:06 +0800

    新增员工详情页路由与跳转逻辑

--- src/router/modules/employees.js
+++ src/router/modules/employees.js
@@ -4,11 +4,19 @@ export default
 {
   path: '/employees',
   component: Layout,
-  children: [{
-    path: '',
-    name: 'Employees',
-    component: () => import('@/views/employees/index'),
-    meta: { title: '员工', icon: 'people' }
-  }]
+  children: [
+    {
+      path: '',
+      name: 'Employees',
+      component: () => import('@/views/employees/index'),
+      meta: { title: '员工', icon: 'people' }
+    },
+    {
+      path: 'detail/:id',
+      hidden: true,
+      component: () => import('@/views/employees/detail'),
+      meta: { title: '员工详情' }
+    }
+  ]
 }
 
