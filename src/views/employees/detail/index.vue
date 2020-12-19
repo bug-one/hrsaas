7d11cd34d51f138619a348d1fb2c61b97953cc57
huangweijie <949801851@qq.com>
2020-12-19 20:48:49 +0800

    新增tab页-登录账户设置页布局

--- src/views/employees/detail/index.vue
+++ src/views/employees/detail/index.vue
@@ -1,12 +1,43 @@
 <template>
-  <div>
-    详情页
+  <div class="dashboard-container">
+    <div class="app-container">
+      <el-card>
+        <el-tabs v-model="activeName">
+          <el-tab-pane label="登录账户设置" name="account">
+            <el-row type="flex" justify="center" style="padding-right: 300px">
+              <el-form label-width="100px">
+                <el-form-item label="姓名">
+                  <el-input />
+                </el-form-item>
+                <el-form-item label="密码">
+                  <el-input />
+                </el-form-item>
+                <el-form-item>
+                  <el-button type="primary">更新</el-button>
+                  <el-button>取消</el-button>
+                </el-form-item>
+              </el-form>
+            </el-row>
+          </el-tab-pane>
+          <el-tab-pane label="个人详情" name="detail">
+            个人详情
+          </el-tab-pane>
+          <el-tab-pane label="岗位信息" name="job">
+            岗位信息
+          </el-tab-pane>
+        </el-tabs>
+      </el-card>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      activeName: 'account'
+    }
+  }
 }
 </script>
 
