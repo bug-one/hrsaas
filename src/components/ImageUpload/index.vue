c54cf7b79fbcba3c76eed7c67f34f49e24c6fa4d
huangweijie <949801851@qq.com>
2020-12-20 21:44:26 +0800

    实现图片预览与图片格式与大小的拦截

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-upload action="#" list-type="picture-card" :file-list="fileList" :on-preview="preview">
+    <el-upload action="#" list-type="picture-card" :file-list="fileList" :on-preview="preview" :class="{'disable':disable}" :on-change="onChange" :http-request="upLoadImage" :on-remove="onRemove" :before-upload="beforeUpload">
       <i class="el-icon-plus" />
     </el-upload>
     <el-dialog :visible.sync="showDialog" title="图片预览">
@@ -15,22 +15,55 @@
 export default {
   data() {
     return {
-      fileList: [
-        { url: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1432014582,710203898&fm=26&gp=0.jpg' }
-      ],
+      fileList: [],
       imgUrl: '',
       showDialog: false
+    }
+  },
+  computed: {
+    disable() {
+      return this.fileList.length === 1
     }
   },
   methods: {
     preview(file) {
       this.imgUrl = file.url
       this.showDialog = true
+    },
+    onChange(file, fileList) {
+      this.fileList = fileList
+    },
+    onRemove(file, fileList) {
+      this.fileList = fileList
+    },
+    beforeUpload(file) {
+      const mixSize = 1024 * 1024 * 1
+      const type = ['image/jpeg', 'image/png']
+      if (type.indexOf(file.type) === -1) {
+        this.$message.error('图片格式仅支持.jpeg.png')
+        return false
+      }
+      if (file.size > mixSize) {
+        this.$message.error('图片大小不能超过1M')
+        return false
+      }
+      return true
+    },
+    upLoadImage(params) {
+      console.log('自定义上传↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓')
+      console.log(params)
     }
   }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.disable {
+::v-deep .el-upload--picture-card{
+     display: none;
+ }
+::v-deep img{
+     object-fit: cover;
+ }
+}
 </style>
