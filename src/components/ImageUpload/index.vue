7eb6283543fb0abc0fd11411dc128a744e38f668
huangweijie <949801851@qq.com>
2020-12-20 18:07:42 +0800

    实现图片上传组件预览

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -1,12 +1,31 @@
 <template>
-  <el-upload action="#" list-type="picture-card">
-    <i class="el-icon-plus" />
-  </el-upload>
+  <div>
+    <el-upload action="#" list-type="picture-card" :file-list="fileList" :on-preview="preview">
+      <i class="el-icon-plus" />
+    </el-upload>
+    <el-dialog :visible.sync="showDialog" title="图片预览">
+      <img :src="imgUrl" alt="">
+    </el-dialog>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      fileList: [
+        { url: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1432014582,710203898&fm=26&gp=0.jpg' }
+      ],
+      imgUrl: '',
+      showDialog: false
+    }
+  },
+  methods: {
+    preview(file) {
+      this.imgUrl = file.url
+      this.showDialog = true
+    }
+  }
 }
 </script>
 
