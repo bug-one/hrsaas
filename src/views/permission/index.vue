26a3a2f8d395050c371991398b01d3749af43588
huangweijie <949801851@qq.com>
2020-12-22 15:31:46 +0800

    获取权限并渲染

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,16 +1,52 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        权限管理
-      </h2>
+      <PageTools>
+        <template slot="before">
+          <span>权限设置，请谨慎</span>
+        </template>
+        <template slot="after">
+          <el-button>新增权限</el-button>
+        </template>
+      </PageTools>
+      <el-card>
+        <el-table border :data="permissionList">
+          <el-table-column label="权限名称" prop="name" width="160px" />
+          <el-table-column align="center" label="权限标识" prop="code" />
+          <el-table-column align="center" label="权限描述" prop="description" />
+          <el-table-column align="center" label="操作">
+            <template>
+              <el-button type="text">添加</el-button>
+              <el-button type="text">编辑</el-button>
+              <el-button type="text">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import PageTools from '@/components/PageTools'
+import { getPermissionList } from '@/api/permission'
 export default {
-
+  components: {
+    PageTools
+  },
+  data() {
+    return {
+      permissionList: []
+    }
+  },
+  created() {
+    this.getPermissionList()
+  },
+  methods: {
+    async getPermissionList() {
+      this.permissionList = await getPermissionList()
+    }
+  }
 }
 </script>
 
